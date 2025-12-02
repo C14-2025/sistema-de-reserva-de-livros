81553b81363bdf2f29866b36eb81f1bb193e42f7
Maria Rita <mariaritaraposo@outlook.com>
2025-12-02 18:41:51 -0300

    fix;jenkins file

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,39 +1,14 @@
 module.exports = {
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['/node_modules/'],
   testMatch: ['**/tests/**/*.test.js'],
-  collectCoverageFrom: [
-    'controllers/**/*.js',
-    'models/**/*.js',
-    'middlewares/**/*.js',
-    '!**/node_modules/**'
-  ],
-  coverageDirectory: 'coverage',
   verbose: true,
   testTimeout: 10000,
 
-  // CONFIGURAÇÃO CORRIGIDA PARA JENKINS
   reporters: [
     'default',
-    [
-      'jest-junit',
-      {
-        outputDirectory: 'test-results',
-        outputName: 'jest-results.xml',
-        suiteName: 'Backend Tests', // Nome fixo, não use ${JOB_NAME}
-        classNameTemplate: '{classname} - {title}',
-        titleTemplate: '{classname} - {title}',
-        ancestorSeparator: ' › ',
-        usePathForSuiteName: 'true',
-        addFileAttribute: 'true',
-        includeConsoleOutput: 'true', // Importante para logs
-        suiteNameTemplate: '{filename}',
-        reportTestSuiteErrors: 'true'
-      }
-    ]
-  ],
-  
-  // Adicione para melhor compatibilidade
-  testLocationInResults: true,
-  coverageReporters: ['text', 'lcov', 'clover', 'html']
+    ['jest-junit', {
+      outputDirectory: 'reports',
+      outputName: 'junit.xml'
+    }]
+  ]
 };
